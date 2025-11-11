605db1e8f2cc8a2ae44f000c40841d946de53e04
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-11 10:20:07 +0800

    fix(dependencies): correct formatting in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,9 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
