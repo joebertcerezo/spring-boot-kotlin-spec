921a34400a16e3f33a2e4df0321665fbe4696e53
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-11 10:51:39 +0800

    feat(controller): implement HealthCheckController for server health check

--- src/main/kotlin/com/cerezo/specification/controllers/HealthCheckController.kt
+++ src/main/kotlin/com/cerezo/specification/controllers/HealthCheckController.kt
@@ -1,4 +1,15 @@
 package com.cerezo.specification.controllers
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HealthCheckController {
+  
+  @GetMapping
+  fun index(): ResponseEntity<String> =
+    ResponseEntity.status(HttpStatus.OK).body("Server running...")
 }
