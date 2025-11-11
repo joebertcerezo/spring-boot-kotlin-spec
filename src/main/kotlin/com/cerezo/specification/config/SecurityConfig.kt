df29c60327c239d37d0f6452272d645244f14584
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-11 10:40:18 +0800

    feat(security): add SecurityConfig for HTTP security configuration

---
+++ src/main/kotlin/com/cerezo/specification/config/SecurityConfig.kt
@@ -0,0 +1,18 @@
+package com.cerezo.specification.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .cors { }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .build()
+}
