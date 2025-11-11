8358a44c5f65bf362ac05dc134b181853b9b102b
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-11 10:10:36 +0800

    feat(application): create SpecificationApplication for Spring Boot initialization

---
+++ src/main/kotlin/com/cerezo/specification/SpecificationApplication.kt
@@ -0,0 +1,11 @@
+package com.cerezo.specification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpecificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpecificationApplication>(*args)
+}
