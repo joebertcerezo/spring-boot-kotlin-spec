853917d6f265f676a28ee6f55bcf31bbf1cf4d68
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-11 10:09:34 +0800

    feat(tests): add initial specification application tests

---
+++ src/test/kotlin/com/cerezo/specification/SpecificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cerezo.specification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpecificationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
