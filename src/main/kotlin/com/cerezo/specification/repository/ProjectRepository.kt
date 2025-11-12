4fea19a7d7f2caa07832d972e7f07b7ad4969569
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-12 16:38:38 +0800

    refactor(repository): remove JpaSpecificationExecutor from ProjectRepository interface

--- src/main/kotlin/com/cerezo/specification/repository/ProjectRepository.kt
+++ src/main/kotlin/com/cerezo/specification/repository/ProjectRepository.kt
@@ -2,9 +2,6 @@ package com.cerezo.specification.repository
 
 import com.cerezo.specification.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import java.util.UUID
 
-interface ProjectRepository :
-  JpaRepository<Project, UUID>,
-  JpaSpecificationExecutor<Project>
+interface ProjectRepository : JpaRepository<Project, UUID>
