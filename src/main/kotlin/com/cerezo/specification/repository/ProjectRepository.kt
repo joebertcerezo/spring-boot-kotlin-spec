c5382fee94017c167d7cc27c8961ef3c398ff229
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-12 16:36:14 +0800

    feat(repository): implement ProjectRepository interface extending JpaRepository and JpaSpecificationExecutor

--- src/main/kotlin/com/cerezo/specification/repository/ProjectRepository.kt
+++ src/main/kotlin/com/cerezo/specification/repository/ProjectRepository.kt
@@ -1,4 +1,10 @@
 package com.cerezo.specification.repository
 
-interface ProjectRepository {
-}
+import com.cerezo.specification.entity.Project
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import java.util.UUID
+
+interface ProjectRepository :
+  JpaRepository<Project, UUID>,
+  JpaSpecificationExecutor<Project>
