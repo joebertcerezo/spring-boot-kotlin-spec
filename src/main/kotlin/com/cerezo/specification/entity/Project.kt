2bca4ff9306fafb074408398c359d43e9aa96ef5
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-12 16:19:04 +0800

    feat(entity): implement Project class with properties and JPA annotations

--- src/main/kotlin/com/cerezo/specification/entity/Project.kt
+++ src/main/kotlin/com/cerezo/specification/entity/Project.kt
@@ -1,4 +1,40 @@
 package com.cerezo.specification.entity
 
-class Project {
-}
+import com.cerezo.specification.constant.ProjectStatus
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "projects")
+class Project(
+  @Id
+  @Column(
+    name = "id",
+    nullable = false,
+    unique = true,
+  )
+  val id: UUID = UUID.randomUUID(),
+  @Column(
+    name = "name",
+    nullable = false,
+  )
+  val name: String,
+  @Column(
+    name = "status",
+    nullable = false,
+  )
+  val status: ProjectStatus = ProjectStatus.TODO,
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(
+    name = "owner_id",
+    foreignKey = ForeignKey(name = "projects_owner_id_fkey"),
+  )
+  val user: User,
+)
