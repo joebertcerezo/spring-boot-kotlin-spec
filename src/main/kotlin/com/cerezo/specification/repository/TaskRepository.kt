72b4dca40bf33bb89d84e5d52e8d46691f0130b9
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-12 16:38:41 +0800

    feat(repository): define TaskRepository interface extending JpaRepository and JpaSpecificationExecutor

--- src/main/kotlin/com/cerezo/specification/repository/TaskRepository.kt
+++ src/main/kotlin/com/cerezo/specification/repository/TaskRepository.kt
@@ -1,4 +1,10 @@
 package com.cerezo.specification.repository
 
-interface TaskRepository {
-}
+import com.cerezo.specification.entity.Task
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import java.util.UUID
+
+interface TaskRepository :
+  JpaRepository<Task, UUID>,
+  JpaSpecificationExecutor<Task>
