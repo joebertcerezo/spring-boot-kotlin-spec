0dfa4f2ca9aba051b71ee6dbd5f702cd73a236d2
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-12 16:19:08 +0800

    feat(entity): define User class with JPA annotations and properties

--- src/main/kotlin/com/cerezo/specification/entity/User.kt
+++ src/main/kotlin/com/cerezo/specification/entity/User.kt
@@ -1,4 +1,30 @@
-package com.cerezo.specification.Entity
+package com.cerezo.specification.entity
 
-class User {
-}
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "users")
+class User(
+  @Id
+  @Column(
+    name = "id",
+    nullable = false,
+    unique = true,
+  )
+  val id: UUID = UUID.randomUUID(),
+  @Column(
+    name = "name",
+    nullable = false,
+  )
+  val name: String,
+  @Column(
+    name = "email",
+    nullable = false,
+    unique = true,
+  )
+  val email: String,
+)
