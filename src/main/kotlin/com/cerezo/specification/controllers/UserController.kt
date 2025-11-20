bba294dcb3dd0152ed3eb9a6d7047e2cd0c4ae5e
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 08:52:46 +0800

    feat(controller): enhance UserController with validation and request mapping for user creation

--- src/main/kotlin/com/cerezo/specification/controllers/UserController.kt
+++ src/main/kotlin/com/cerezo/specification/controllers/UserController.kt
@@ -3,17 +3,23 @@ package com.cerezo.specification.controllers
 import com.cerezo.specification.dto.request.UserCreateRequest
 import com.cerezo.specification.dto.response.UserResponse
 import com.cerezo.specification.service.UserService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("\${api.base-path}/users")
 class UserController(
   private val userService: UserService,
 ) {
   @PostMapping
-  fun create(userCreateRequest: UserCreateRequest): ResponseEntity<UserResponse> {
+  fun create(
+    @Valid @RequestBody userCreateRequest: UserCreateRequest,
+  ): ResponseEntity<UserResponse> {
     val user = userService.create(userCreateRequest)
     return ResponseEntity.status(HttpStatus.CREATED).body(user)
   }
