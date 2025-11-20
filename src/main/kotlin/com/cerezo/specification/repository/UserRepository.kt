66868c545082f0918751b92b298142ed67a18bcb
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 08:23:14 +0800

    feat(repository): add findByEmail method to UserRepository interface

--- src/main/kotlin/com/cerezo/specification/repository/UserRepository.kt
+++ src/main/kotlin/com/cerezo/specification/repository/UserRepository.kt
@@ -4,4 +4,6 @@ import com.cerezo.specification.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface UserRepository : JpaRepository<User, UUID>
+interface UserRepository : JpaRepository<User, UUID> {
+  fun findByEmail(email: String): User?
+}
