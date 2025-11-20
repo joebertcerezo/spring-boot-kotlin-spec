c3f83e5b1a24a54ba0eb518777a2267352b1f53f
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 16:03:15 +0800

    feat(controller): implement TaskController for task creation

--- src/main/kotlin/com/cerezo/specification/controllers/TaskController.kt
+++ src/main/kotlin/com/cerezo/specification/controllers/TaskController.kt
@@ -1,4 +1,26 @@
 package com.cerezo.specification.controllers
 
-class TaskController {
+import com.cerezo.specification.dto.request.TaskCreateRequest
+import com.cerezo.specification.dto.response.TaskResponse
+import com.cerezo.specification.service.TaskService
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("\${api.base-path}/tasks")
+class TaskController(
+  private val taskService: TaskService,
+) {
+  @PostMapping
+  fun create(
+    @Valid @RequestBody taskCreate: TaskCreateRequest,
+  ): ResponseEntity<TaskResponse> {
+    val response = taskService.create(taskCreate)
+    return ResponseEntity.status(HttpStatus.CREATED).body(response)
+  }
 }
