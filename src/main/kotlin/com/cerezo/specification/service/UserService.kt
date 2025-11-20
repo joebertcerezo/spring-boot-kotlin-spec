b10ca40c18f24784c92c713e66492e15b0d8c051
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 08:23:17 +0800

    feat(service): implement create method in UserService for user registration

--- src/main/kotlin/com/cerezo/specification/service/UserService.kt
+++ src/main/kotlin/com/cerezo/specification/service/UserService.kt
@@ -1,12 +1,25 @@
 package com.cerezo.specification.service
 
-import com.cerezo.specification.controllers.HealthCheckController
 import com.cerezo.specification.dto.request.UserCreateRequest
+import com.cerezo.specification.dto.response.UserResponse
+import com.cerezo.specification.dto.response.toResponse
+import com.cerezo.specification.entity.User
+import com.cerezo.specification.repository.UserRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class UserService(
-  private val userController: HealthCheckController
+  private val userRepository: UserRepository,
 ) {
-  fun create(user: UserCreateRequest): 
+  fun create(userCreate: UserCreateRequest): UserResponse {
+    val findUser = userRepository.findByEmail(userCreate.email)
+    if (findUser != null) {
+      throw ResponseStatusException(HttpStatus.BAD_REQUEST, "User email already exist.")
+    }
+
+    val user = User(name = userCreate.name, email = userCreate.email)
+    return userRepository.save(user).toResponse()
+  }
 }
