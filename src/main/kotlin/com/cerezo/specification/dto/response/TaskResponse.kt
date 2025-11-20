d934fa816b8b3bf41d14577e026f472220b88e81
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 15:49:43 +0800

    feat(dto): implement TaskResponse data class and extension function for Task entity

--- src/main/kotlin/com/cerezo/specification/dto/response/TaskResponse.kt
+++ src/main/kotlin/com/cerezo/specification/dto/response/TaskResponse.kt
@@ -1,3 +1,26 @@
 package com.cerezo.specification.dto.response
 
-data class TaskResponse()
+import com.cerezo.specification.entity.Project
+import com.cerezo.specification.entity.Task
+import com.cerezo.specification.entity.User
+import java.time.Instant
+import java.util.UUID
+
+data class TaskResponse(
+  val id: UUID,
+  val title: String,
+  val description: String?,
+  val dueDate: Instant?,
+  val assignee: User,
+  val project: Project,
+)
+
+fun Task.toResponse(): TaskResponse =
+  TaskResponse(
+    id = this.id,
+    title = this.title,
+    description = this.description,
+    dueDate = this.dueDate,
+    assignee = this.user,
+    project = this.project,
+  )
