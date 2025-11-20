cdf4cc8d144290152821b7a2ff63b9ed72b08078
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 08:12:17 +0800

    feat(service): implement UserService class with HealthCheckController dependency

--- src/main/kotlin/com/cerezo/specification/service/UserService.kt
+++ src/main/kotlin/com/cerezo/specification/service/UserService.kt
@@ -1,4 +1,12 @@
 package com.cerezo.specification.service
 
-class UserService {
+import com.cerezo.specification.controllers.HealthCheckController
+import com.cerezo.specification.dto.request.UserCreateRequest
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+  private val userController: HealthCheckController
+) {
+  fun create(user: UserCreateRequest): 
 }
