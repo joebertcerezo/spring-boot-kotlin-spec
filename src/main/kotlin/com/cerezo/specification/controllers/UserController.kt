b012eaf9f631498fecc0c99c089fbf81b6a20956
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 08:23:04 +0800

    feat(controller): implement UserController with create endpoint for user creation

--- src/main/kotlin/com/cerezo/specification/controllers/UserController.kt
+++ src/main/kotlin/com/cerezo/specification/controllers/UserController.kt
@@ -1,4 +1,20 @@
 package com.cerezo.specification.controllers
 
-class UserController {
+import com.cerezo.specification.dto.request.UserCreateRequest
+import com.cerezo.specification.dto.response.UserResponse
+import com.cerezo.specification.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController(
+  private val userService: UserService,
+) {
+  @PostMapping
+  fun create(userCreateRequest: UserCreateRequest): ResponseEntity<UserResponse> {
+    val user = userService.create(userCreateRequest)
+    return ResponseEntity.status(HttpStatus.CREATED).body(user)
+  }
 }
