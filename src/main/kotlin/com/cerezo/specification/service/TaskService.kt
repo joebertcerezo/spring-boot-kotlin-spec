6e741ab81ad42d29ad61eeac20837b8e0c75639d
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 16:03:18 +0800

    feat(service): implement TaskService for task creation and management

--- src/main/kotlin/com/cerezo/specification/service/TaskService.kt
+++ src/main/kotlin/com/cerezo/specification/service/TaskService.kt
@@ -1,4 +1,42 @@
 package com.cerezo.specification.service
 
-class TaskService {
+import com.cerezo.specification.dto.request.TaskCreateRequest
+import com.cerezo.specification.dto.response.TaskResponse
+import com.cerezo.specification.dto.response.toResponse
+import com.cerezo.specification.entity.Task
+import com.cerezo.specification.repository.ProjectRepository
+import com.cerezo.specification.repository.TaskRepository
+import com.cerezo.specification.repository.UserRepository
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class TaskService(
+  private val taskRepository: TaskRepository,
+  private val userRepository: UserRepository,
+  private val projectRepository: ProjectRepository,
+) {
+  fun create(taskCreate: TaskCreateRequest): TaskResponse {
+    val findUser =
+      userRepository
+        .findById(
+          taskCreate.userId,
+        ).orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
+    val findProject =
+      projectRepository.findById(taskCreate.projectId).orElseThrow {
+        ResponseStatusException(HttpStatus.NOT_FOUND)
+      }
+
+    return taskRepository
+      .save(
+        Task(
+          title = taskCreate.title,
+          description = taskCreate.description,
+          dueDate = taskCreate.dueDate,
+          user = findUser,
+          project = findProject,
+        ),
+      ).toResponse()
+  }
 }
