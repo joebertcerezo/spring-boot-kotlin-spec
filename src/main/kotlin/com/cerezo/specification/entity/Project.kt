9e21224ae954f4b79fe6b7818d42ac80eeadf6dc
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 09:07:30 +0800

    feat(entity): add enum mapping for Project status in Project entity

--- src/main/kotlin/com/cerezo/specification/entity/Project.kt
+++ src/main/kotlin/com/cerezo/specification/entity/Project.kt
@@ -3,12 +3,17 @@ package com.cerezo.specification.entity
 import com.cerezo.specification.constant.ProjectStatus
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.ForeignKey
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
+import java.sql.SQLType
 import java.util.UUID
 
 @Entity
@@ -30,6 +35,8 @@ class Project(
     name = "status",
     nullable = false,
   )
+  @Enumerated(EnumType.STRING)
+  @JdbcTypeCode(SqlTypes.NAMED_ENUM)
   val status: ProjectStatus = ProjectStatus.TODO,
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(
