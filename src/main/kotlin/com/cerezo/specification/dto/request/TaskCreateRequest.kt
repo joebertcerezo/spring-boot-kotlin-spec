0666c71792b579a32c441a164b741c85d48d9c81
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-11-20 15:49:39 +0800

    feat(dto): define TaskCreateRequest data class with necessary fields

--- src/main/kotlin/com/cerezo/specification/dto/request/TaskCreateRequest.kt
+++ src/main/kotlin/com/cerezo/specification/dto/request/TaskCreateRequest.kt
@@ -1,3 +1,12 @@
 package com.cerezo.specification.dto.request
 
-data class TaskCreateRequest()
+import java.time.Instant
+import java.util.UUID
+
+data class TaskCreateRequest(
+  val title: String,
+  val description: String?,
+  val dueDate: Instant,
+  val userId: UUID,
+  val projectId: UUID,
+)
