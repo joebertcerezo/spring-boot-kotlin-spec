0a538f835dae9563b75c9d7351eca760d8f52867
sirezo <joebertcerezo@gmail.com>
2025-12-02 20:51:19 +0800

    feat(specification): add hasProjectStatus function to TaskSpec for filtering by project status

--- src/main/kotlin/com/cerezo/specification/specification/TaskSpec.kt
+++ src/main/kotlin/com/cerezo/specification/specification/TaskSpec.kt
@@ -1,4 +1,16 @@
 package com.cerezo.specification.specification
 
+import com.cerezo.specification.constant.ProjectStatus
+import com.cerezo.specification.entity.Task
+import org.springframework.data.jpa.domain.Specification
+
 object TaskSpec {
+  fun hasProjectStatus(status: ProjectStatus?): Specification<Task>? =
+    if(status == null) {
+      null
+    } else {
+      Specification { root, _, builder ->
+        root.get<ProjectStatus>("")
+      }
+  }
 }
