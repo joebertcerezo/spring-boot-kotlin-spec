8a9e858fb158fa2b471b3b37115dfeb9eb174ba2
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-12-08 10:32:57 +0800

    feat(specification): add ProjectFilter class and hasProjectStatus function for task filtering

---
+++ src/main/kotlin/com/cerezo/specification/dto/filters/ProjectFilter.kt
@@ -0,0 +1,4 @@
+package com.cerezo.specification.dto.filters
+
+class ProjectFilter {
+}
--- src/main/kotlin/com/cerezo/specification/specification/ProjectSpec.kt
+++ src/main/kotlin/com/cerezo/specification/specification/ProjectSpec.kt
@@ -4,13 +4,13 @@ import com.cerezo.specification.constant.ProjectStatus
 import com.cerezo.specification.entity.Task
 import org.springframework.data.jpa.domain.Specification
 
-object TaskSpec {
+object ProjectSpec {
   fun hasProjectStatus(status: ProjectStatus?): Specification<Task>? =
-    if(status == null) {
+    if (status == null) {
       null
     } else {
       Specification { root, _, builder ->
         root.get<ProjectStatus>("")
       }
-  }
+    }
 }
